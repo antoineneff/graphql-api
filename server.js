b9ed1ebecada6c3a026037cc96f916ba268dcced
Antoine Neff <antoineneff@me.com>
2017-06-11 16:19:23 +0200

    Graphql sample with graphiql interface

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import { buildSchema } from 'graphql';
+import graphqlHTTP from 'express-graphql';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Api is working !');
-});
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+const root = {
+    hello: () => {
+        return 'Hello world !';
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(1337, () => {
-    console.log('Server started, listening on http://localhost:1337');
+    console.log('Server started, listening on http://localhost:1337/graphql');
 });
