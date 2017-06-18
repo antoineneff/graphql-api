33877802f080eb069fdf7f6bd754bb1482813694
Antoine Neff <antoineneff@me.com>
2017-06-18 06:23:41 +0200

    Update user schema : import instead of require

--- mongoose/user.js
+++ mongoose/user.js
@@ -1,5 +1,4 @@
-const mongoose  = require('mongoose');
-const Schema    = mongoose.Schema;
+import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
     email       : String,
